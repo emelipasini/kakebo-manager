c26a0e1af9bb4b3855d164b3d7d431c0f09759ef
Emelí Pasini <emelipasini@outlook.com>
2023-07-21 16:19:11 -0300

    feat: add get all and get by month for expenses and savings

--- src/services/expense.ts
+++ src/services/expense.ts
@@ -1,15 +1,31 @@
 import { resolve, dirname } from "node:path";
-import { writeFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 
-import Expense from "../models/expense.js";
+import Expense from "../types/expense.js";
 
 import type ExpenseType from "../types/expense-type.js";
 
 const DB_PATH = resolve(dirname("."), "database/expenses.json");
 
+const getExpenses = (): Expense[] => {
+    const data: string = readFileSync(DB_PATH, "utf8");
+    const expenses: Expense[] = JSON.parse(data) as Expense[];
+    return expenses;
+};
+
+export const getMonthExpenses = (): Expense[] => {
+    const expenses = getExpenses();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    return expenses.filter(
+        (expense) => new Date(expense.date).getMonth() === month && new Date(expense.date).getFullYear() === year
+    );
+};
+
 export const saveExpense = (description: string, amount: number, expenseType: ExpenseType): Expense => {
     const newExpense = new Expense(description, amount, expenseType);
-    const data = JSON.stringify(newExpense);
-    writeFileSync(DB_PATH, data);
+    const expenses = getExpenses();
+    expenses.push(newExpense);
+    writeFileSync(DB_PATH, JSON.stringify(expenses));
     return newExpense;
 };
--- src/services/saving.ts
+++ src/services/saving.ts
@@ -1,13 +1,29 @@
 import { resolve, dirname } from "node:path";
-import { writeFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 
-import Saving from "../models/saving.js";
+import Saving from "../types/saving.js";
 
 const DB_PATH = resolve(dirname("."), "database/savings.json");
 
+const getSavings = (): Saving[] => {
+    const data: string = readFileSync(DB_PATH, "utf8");
+    const savings: Saving[] = JSON.parse(data) as Saving[];
+    return savings;
+};
+
+export const getMonthSavings = (): Saving[] => {
+    const savings = getSavings();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    return savings.filter(
+        (saving) => new Date(saving.date).getMonth() === month && new Date(saving.date).getFullYear() === year
+    );
+};
+
 export const saveSaving = (description: string, amount: number): Saving => {
     const newSaving = new Saving(amount, description);
-    const data = JSON.stringify(newSaving);
-    writeFileSync(DB_PATH, data);
+    const savings = getSavings();
+    savings.push(newSaving);
+    writeFileSync(DB_PATH, JSON.stringify(savings));
     return newSaving;
 };
